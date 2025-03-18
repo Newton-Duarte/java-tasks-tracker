4259655b1dec29fe5fcbb6e3ed43e61ea968ff11
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-18 20:20:37 -0300

    feat: add task mapper implementation

---
+++ src/main/java/com/newtonduarte/tasks/mappers/impl/TaskMapperImpl.java
@@ -0,0 +1,36 @@
+package com.newtonduarte.tasks.mappers.impl;
+
+import com.newtonduarte.tasks.domain.dto.TaskDto;
+import com.newtonduarte.tasks.domain.entities.Task;
+import com.newtonduarte.tasks.mappers.TaskMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapperImpl implements TaskMapper {
+    @Override
+    public Task fromDto(TaskDto taskDto) {
+        return new Task(
+                taskDto.id(),
+                taskDto.title(),
+                taskDto.description(),
+                taskDto.dueDate(),
+                taskDto.status(),
+                taskDto.priority(),
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getStatus(),
+                task.getPriority()
+        );
+    }
+}
