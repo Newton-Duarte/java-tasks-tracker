f13ebac388dbf21c08584de30cd44b20ba13be35
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-18 20:32:53 -0300

    feat: add task jpa repository interface

---
+++ src/main/java/com/newtonduarte/tasks/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.newtonduarte.tasks.repositories;
+
+import com.newtonduarte.tasks.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
