14bbd9ad06ef3c1f06a5d607aecd449c86446682
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-19 07:54:06 -0300

    feat: add create task list implementation

--- src/main/java/com/newtonduarte/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/newtonduarte/tasks/services/impl/TaskListServiceImpl.java
@@ -5,6 +5,7 @@ import com.newtonduarte.tasks.repositories.TaskListRepository;
 import com.newtonduarte.tasks.services.TaskListService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,5 +19,29 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public List<TaskList> getTaskLists() {
         return taskListRepository.findAll();
+    }
+
+    @Override
+    public TaskList createTaskList(TaskList taskList) {
+        if (null != taskList.getId()) {
+            throw new IllegalArgumentException("Task list already has an ID");
+        }
+
+        if (null == taskList.getTitle() || taskList.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task list title must be present!");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskListRepository.save(
+                new TaskList(
+                        null,
+                        taskList.getTitle(),
+                        taskList.getDescription(),
+                        null,
+                        now,
+                        now
+                )
+        );
     }
 }
