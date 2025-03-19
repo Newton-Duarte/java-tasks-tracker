38c4b4a63dbae2f65df2e281cfb64def21a3d271
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-19 08:11:48 -0300

    feat: add exception handler for entity not found exceptions

--- src/main/java/com/newtonduarte/tasks/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/newtonduarte/tasks/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.newtonduarte.tasks.controllers;
 
 import com.newtonduarte.tasks.domain.dto.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({IllegalArgumentException.class})
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleExceptions(
             RuntimeException ex, WebRequest request
     ) {
@@ -21,5 +22,16 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
