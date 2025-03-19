027b69d77632d4b3ea19937a73673185619b72c5
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-19 08:07:41 -0300

    feat: add get task list by id method

--- src/main/java/com/newtonduarte/tasks/services/TaskListService.java
+++ src/main/java/com/newtonduarte/tasks/services/TaskListService.java
@@ -3,8 +3,10 @@ package com.newtonduarte.tasks.services;
 import com.newtonduarte.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> getTaskLists();
+    TaskList getTaskList(UUID id);
     TaskList createTaskList(TaskList taskList);
 }
