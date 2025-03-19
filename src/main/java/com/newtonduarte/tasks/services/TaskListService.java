71b46c10f2d35da4e8723736d751079dcdf43c32
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-19 07:41:44 -0300

    feat: add task list service interface

---
+++ src/main/java/com/newtonduarte/tasks/services/TaskListService.java
@@ -0,0 +1,9 @@
+package com.newtonduarte.tasks.services;
+
+import com.newtonduarte.tasks.domain.entities.TaskList;
+
+import java.util.List;
+
+public interface TaskListService {
+    List<TaskList> getTaskLists();
+}
